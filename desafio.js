7f0cd025dfa4a9072770acf7a2cffec8a147bfbf
German Perrachione <perrachione.g@gmail.com>
2023-01-23 22:07:21 -0300

    version1

--- desafio.js
+++ desafio.js
@@ -1,6 +1,7 @@
-class ProductManager{
 
-    static products= [];
+class Product{
+
+    static id=0;
 
     constructor(titulo, descripcion,precio,imagen,codigo,stock){
         this.titulo = titulo;
@@ -9,32 +10,51 @@ class ProductManager{
         this.imagen = imagen;
         this.codigo = codigo;
         this.stock = stock;
-        this.#id = id;
+        this.id = Product.id++;
 
-    }
+}
 
-    addproduct(product){
-
-    if (ProductManager.products.includes(product.id))
-        return "codigo de producto repetido"
-        ProductManager.products.push(product)
+};
 
 
-    }
+    class ProductManager{
 
+        constructor(){
+
+        this.products = new Array()
+
+}
+
+    addProduct(product){
+
+        if(this.products.some(p => p.codigo === product.codigo)) 
+            return "Error: Código de producto repetido"
+            this.products.push(product);
+
+}
 
     getProducts() {
 
         return this.products
 
-  }
+}
 
 
     getProductById(id) {
 
         const productById = this.products.filter(p => p.id === id)
 
-  }
+}
 
 };
 
+
+
+let usuario = new Product('titulo', 'descripcion','precio','imagen','codigo','stock');
+
+Product.addProduct('latte')
+Product.addProduct('cookies')
+Product.addProduct('coffee')
+
+
+console.log(Array);
