5dec76aa065d4bde7f9e41b0ae56e11005926c5a
German Perrachione <perrachione.g@gmail.com>
2023-01-24 20:47:41 -0300

    v2

--- desafio.js
+++ desafio.js
@@ -23,13 +23,16 @@ class Product{
 
         this.products = new Array()
 
+        
 }
 
-    addProduct = (product)=>{
+    addProduct = (titulo,descripcion,precio,imagen,codigo,stock)=>{
+        
 
-        if(this.products.some(p => p.codigo === product.codigo)) 
+        if(this.products.some(p => p.codigo === Product.codigo)) 
             return "Error: Código de producto repetido"
-            this.products.push(product);
+            this.products.push(titulo,descripcion,precio,imagen,codigo,stock);
+
 
 }
 
@@ -55,9 +58,10 @@ class Product{
 
 
 const gestor = new ProductManager()
+
 console.log(gestor.getProducts()); //Me devuelve un array vacio.
 
 gestor.addProduct("producto prueba", "Este es un producto prueba", 200,"Sin imagen", "abc123", 25);
 console.log(gestor.getProducts());
 gestor.addProduct("producto prueba", "Este es un producto prueba", 200,"Sin imagen", "abc123", 25);
-
+console.log(gestor.getProducts());
